ddcc7f8ee327431ff87adac1e98e3e87e6323524
vivekssss <vivektukaramsalgaonkar1212@gmail.com>
2025-10-31 13:37:54 +0530

    feat: amazon clone phase 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Amazon-clone',
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
